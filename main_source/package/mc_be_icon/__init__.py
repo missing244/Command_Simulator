54a26d00a79fdaa96a9360de7534ae9d1e1f7faa
missing244 <112210092+missing244@users.noreply.github.com>
2025-01-15 23:04:19 +0800

    Delete main_source/package/mc_be_icon directory

--- main_source/package/mc_be_icon/__init__.py
+++
@@ -1,9 +0,0 @@
-import zipfile,tkinter,os
-now_dir = os.path.realpath(os.path.join(__file__, os.pardir))
-
-icon_zip_file = zipfile.ZipFile(os.path.join(now_dir,"icon.zip"),"r")
-icon_list = icon_zip_file.namelist()
-icon_zip_file.extractall(os.path.join(now_dir,"icon"))
-
-def tk_Image(name : str) :
-    return tkinter.PhotoImage(file=os.path.join(now_dir,"icon",name))
