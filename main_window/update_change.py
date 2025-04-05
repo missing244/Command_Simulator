6ccf51f630f0600d4a07186ca1910c2d08980825
missing244 <112210092+missing244@users.noreply.github.com>
2025-04-05 21:19:03 +0800

    Delete main_window directory

--- main_window/update_change.py
+++
@@ -1,8 +0,0 @@
-import package.file_operation as file_IO
-import os, sys
-
-OLD_NBT_PATH = os.path.join("main_source", "package", "python_nbt.py")
-if file_IO.is_file(OLD_NBT_PATH) : os.remove(OLD_NBT_PATH)
-
-OLD_BDX_PATH = os.path.join("main_source", "package", "python_bdx")
-if file_IO.is_dir(OLD_BDX_PATH) : os.remove(OLD_BDX_PATH)
