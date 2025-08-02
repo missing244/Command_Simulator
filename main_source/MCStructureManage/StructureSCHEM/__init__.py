9372471d8c5f40c69e8dd5d19fba6abdec338d9b
missing244 <112210092+missing244@users.noreply.github.com>
2025-08-02 21:28:57 +0800

    Delete main_source/MCStructureManage directory

--- main_source/MCStructureManage/StructureSCHEM/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-    schem 文件解析模块
-    ---------------------------------
-    * 可用对象 Schematic: 解析schematic文件的类
-    * 可用属性 RuntimeID_to_Block: 数字ID-方块 映射列表
-    * 可用属性 Block_to_RuntimeID: 方块-数字ID 映射字典
-"""
-
-from .schem import Schem_V1, Schem_V2
--- main_source/MCStructureManage/StructureSCHEMATIC/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-    schematic 文件解析模块
-    ---------------------------------
-    * 可用对象 Schematic: 解析schematic文件的类
-    * 可用属性 RuntimeID_to_Block: 数字ID-方块 映射列表
-    * 可用属性 Block_to_RuntimeID: 方块-数字ID 映射字典
-"""
-
-from .schematic import RuntimeID_to_Block , Block_to_RuntimeID, Schematic
